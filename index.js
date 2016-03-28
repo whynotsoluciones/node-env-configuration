96b28c2e94fbdac8993a184e0c35141d96f223cf
Ezequiel Cuñado <ecunado@gmail.com>
2016-03-28 13:15:46 +0200

    Include jshint directives and jshintrc config file

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
+/*jslint node: true */
+"use strict";
+
 var Config = require('./lib/config'),
-	changeCase = require('change-case');
+  changeCase = require('change-case');
 
 var SEPARATOR = '_';
 
 module.exports = function (appName, defaultConfig, debug) {
 
-	// Get env vars prefix based on app name
-	appName = appName || '';
-	var config = new Config(defaultConfig);
-	var prefix = changeCase.constantCase(appName);
-	var debug = typeof debug === 'function' ? debug : function() {};
+  // Get env vars prefix based on app name
+  appName = appName || '';
+  var config = new Config(defaultConfig);
+  var prefix = changeCase.constantCase(appName);
+  var debug = typeof debug === 'function' ? debug : function () {};
 
-	// Iterate over env vars
-	var hierarchy = [];
-	var env;
-	var value;
-	for (env in process.env) {
-		// if env is in app namespace
-		if (env.indexOf(prefix) === 0) {
-			// split each var using underscore as separator
-			hierarchy = env.replace(prefix !== '' ?
-				prefix + '_' : prefix, '').split(SEPARATOR);
-			try {
-				value = JSON.parse(process.env[env]);
-			} catch (error) {
-				value = process.env[env];
-				debug(error.message);
-			}
-			try {
-				config.extend(hierarchy, value);
-			} catch (error) {
-				debug(error.message);
-			}
-		}
-	}
+  // Iterate over env vars
+  var hierarchy = [];
+  var env;
+  var value;
+  for (env in process.env) {
+    // if env is in app namespace
+    if (env.indexOf(prefix) === 0) {
+      // split each var using underscore as separator
+      hierarchy = env.replace(prefix !== '' ?
+        prefix + '_' : prefix, '').split(SEPARATOR);
+      try {
+        value = JSON.parse(process.env[env]);
+      } catch (error) {
+        value = process.env[env];
+        debug(error.message);
+      }
+      try {
+        config.extend(hierarchy, value);
+      } catch (error) {
+        debug(error.message);
+      }
+    }
+  }
 
-	return config.conf;
+  return config.conf;
 
 };
--- test/test.js
+++ test/test.js
@@ -1,3 +1,6 @@
+/*jslint node: true */
+"use strict";
+
 /**
  * Set required env vars for appName
  */
@@ -29,71 +32,71 @@ process.env.APP3_OBJ4_VAR8 = '4.1';
 // process.env.APP3_OBJ4_OBJ41_VAR82 = 'Obj 4 Obj 41 var 82 value';
 
 var DEFAULTS = {
-    obj4: {
-        obj41: {
-            var82: 'Obj 4 Obj 41 var 82 value'
-        }
+  obj4: {
+    obj41: {
+      var82: 'Obj 4 Obj 41 var 82 value'
     }
+  }
 };
 
 var assert = require('assert'),
-    configApp1 = require('./../index')('app1'),
-    configApp2 = require('./../index')('app2'),
-    configApp3 = require('./../index')('app3', DEFAULTS);
+  configApp1 = require('./../index')('app1'),
+  configApp2 = require('./../index')('app2'),
+  configApp3 = require('./../index')('app3', DEFAULTS);
 
 describe('Read configuration from environment', function () {
 
-    it('should retrieve configuration for app1 from environment', function () {
-        assert.deepEqual(configApp1, {
-            var1: process.env.APP1_VAR1,
-            var2: 100,
-            obj1: {
-                var1: process.env.APP1_OBJ1_VAR1,
-                var2: process.env.APP1_OBJ1_VAR2
-            }
-        });
+  it('should retrieve configuration for app1 from environment', function () {
+    assert.deepEqual(configApp1, {
+      var1: process.env.APP1_VAR1,
+      var2: 100,
+      obj1: {
+        var1: process.env.APP1_OBJ1_VAR1,
+        var2: process.env.APP1_OBJ1_VAR2
+      }
     });
+  });
 
-    it('should retrieve configuration for app2 from environment', function () {
-        assert.deepEqual(configApp2, {
-            var1: 1.3,
-            var2: process.env.APP2_VAR2,
-            obj1: {
-                var1: process.env.APP2_OBJ1_VAR1,
-                var2: process.env.APP2_OBJ1_VAR2
-            },
-            obj2: {
-                obj21: {
-                    var1: process.env.APP2_OBJ2_OBJ21_VAR1
-                },
-                obj22: {
-                    var1: process.env.APP2_OBJ2_OBJ22_VAR1
-                }
-            }
-        });
+  it('should retrieve configuration for app2 from environment', function () {
+    assert.deepEqual(configApp2, {
+      var1: 1.3,
+      var2: process.env.APP2_VAR2,
+      obj1: {
+        var1: process.env.APP2_OBJ1_VAR1,
+        var2: process.env.APP2_OBJ1_VAR2
+      },
+      obj2: {
+        obj21: {
+          var1: process.env.APP2_OBJ2_OBJ21_VAR1
+        },
+        obj22: {
+          var1: process.env.APP2_OBJ2_OBJ22_VAR1
+        }
+      }
     });
+  });
 
-    it('should retrieve configuration for app3 from environment', function () {
-        assert.deepEqual(configApp3, {
-            var1: true,
-            var2: process.env.APP3_VAR2,
-            obj1: {
-                var1: process.env.APP3_OBJ1_VAR1,
-                var2: process.env.APP3_OBJ1_VAR2
-            },
-            obj2: {
-                var2: process.env.APP3_OBJ2_VAR2
-            },
-            obj3: {
-                var6: process.env.APP3_OBJ3_VAR6
-            },
-            obj4: {
-                var8: process.env.APP3_OBJ4_VAR8,
-                obj41: {
-                    var82: DEFAULTS.obj4.obj41.var82
-                }
-            }
-        });
+  it('should retrieve configuration for app3 from environment', function () {
+    assert.deepEqual(configApp3, {
+      var1: true,
+      var2: process.env.APP3_VAR2,
+      obj1: {
+        var1: process.env.APP3_OBJ1_VAR1,
+        var2: process.env.APP3_OBJ1_VAR2
+      },
+      obj2: {
+        var2: process.env.APP3_OBJ2_VAR2
+      },
+      obj3: {
+        var6: process.env.APP3_OBJ3_VAR6
+      },
+      obj4: {
+        var8: process.env.APP3_OBJ4_VAR8,
+        obj41: {
+          var82: DEFAULTS.obj4.obj41.var82
+        }
+      }
     });
+  });
 
 });
